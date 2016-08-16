8b02c6d3c88a419b93480b8ec62c6e3f3f7fa4bc
saadaouad <aouadsaad05@gmail.com>
2016-08-16 20:26:52 +0100

    Adding mongodb to the app

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+
 var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended : true
-}))
+    extended: true
+}));
 
-car cats = require('./cats.js')(app);
+var cats = require('./cat_routes.js')(app);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var server = app.listen(3000, function () {
+    console.log('Server running at http://127.0.0.1:3000/');
 });
